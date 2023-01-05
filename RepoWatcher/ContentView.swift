8bb72baacab847c22d11fcea4e7a17a538f3d006
Thomas <tommyprezioso@gmail.com>
2023-01-04 22:03:59 -0500

    Initial Commit

---
+++ RepoWatcher/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RepoWatcher
+//
+//  Created by Thomas Prezioso Jr on 1/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Thomas Prezioso Jr on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
