3624b1c8a235c59b5e8020dc6f1bcc287d98c44a
Thomas <tommyprezioso@gmail.com>
2023-01-04 22:08:30 -0500

    first commit

---
+++ RepoWatcherWidget/RepoWatcherWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RepoWatcherWidgetBundle.swift
+//  RepoWatcherWidget
+//
+//  Created by Thomas Prezioso Jr on 1/4/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RepoWatcherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RepoWatcherWidget()
+    }
+}
