5ea59dae864c2a441c57abf3918f757f0b9506df
Thomas <tommyprezioso@gmail.com>
2023-01-18 20:13:24 -0500

    added list and repo save

--- RepoWatcher/ContentView.swift
+++ RepoWatcher/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newRepo = ""
+    @State private var repos: [String] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("Ex. sallen0400/swift-news", text: $newRepo)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                        .textFieldStyle(.roundedBorder)
+
+                    Button {
+                        if !repos.contains(newRepo) && !newRepo.isEmpty {
+                            repos.append(newRepo)
+                            UserDefaults.shared.set(repos, forKey: UserDefaults.repoKey)
+                            newRepo = ""
+                        } else {
+                            print("Repo already exists or array is empty")
+                        }
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.green)
+                    }
+                }
+                .padding()
+
+                VStack(alignment: .leading) {
+                    Text("Saved Repos")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                        .padding(.leading)
+
+                    List(repos, id: \.self) { repo in
+                        Text(repo)
+                            .swipeActions {
+                                Button("Delete") {
+                                    if repos.count > 1 {
+                                        repos.removeAll { $0 == repo }
+                                        UserDefaults.shared.set(repos, forKey: UserDefaults.repoKey)
+                                    }
+                                }
+                                .tint(.red)
+                            }
+                    }
+                }
+            }
+            .navigationTitle("Repo List")
+            .onAppear {
+                guard let retrievedRepos = UserDefaults.shared.value(forKey: UserDefaults.repoKey) as? [String] else {
+                    let defaultValue = ["sallen0400/swift-news"]
+                    UserDefaults.shared.set(defaultValue, forKey: UserDefaults.repoKey)
+                    repos = defaultValue
+                    return
+                }
+                
+                repos = retrievedRepos
+            }
         }
-        .padding()
     }
 }
 
---
+++ RepoWatcher/UserDefaults+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaults+Ext.swift
+//  RepoWatcher
+//
+//  Created by Thomas Prezioso Jr on 1/18/23.
+//
+
+import Foundation
+
+extension UserDefaults {
+    static var shared: UserDefaults {
+        UserDefaults(suiteName: "group.com.Swifttom.RepoWatcher")!
+    }
+    
+    static let repoKey = "repos"
+}
